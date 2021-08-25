5a8e4a68913dfaf5108d49acf9a71e9fa286acc9
Dabuaf86 <dabuaf@gmail.com>
2021-08-25 16:26:42 -0300

    change of approach from dots to buttons/added server list with json

--- App.js
+++ App.js
@@ -1,27 +1,13 @@
-import React, { Component } from 'react';
-import { View, StyleSheet, Text } from 'react-native';
+import React from 'react';
+import { View } from 'react-native';
 import Swipe from './components/ImageCarousel';
 
-const images = [
-	'https://images.pexels.com/photos/9215861/pexels-photo-9215861.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
-	'https://images.pexels.com/photos/1619317/pexels-photo-1619317.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260',
-	'https://images.pexels.com/photos/592077/pexels-photo-592077.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260',
-	'https://images.pexels.com/photos/814499/pexels-photo-814499.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260',
-	'https://images.pexels.com/photos/417074/pexels-photo-417074.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
-	'https://images.pexels.com/photos/206359/pexels-photo-206359.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260',
-	'https://images.pexels.com/photos/4321085/pexels-photo-4321085.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
-];
+const App = () => {
+	return (
+		<View>
+			<Swipe />
+		</View>
+	);
+};
 
-export default class App extends Component {
-	render() {
-		return (
-			<View>
-				<Text>Check your next destination</Text>
-				<Swipe images={images} />
-			</View>
-		);
-	}
-}
-const style = StyleSheet.create({
-	container: { marginTop: 50 },
-});
+export default App;
--- components/ImageCarousel.js
+++ components/ImageCarousel.js
@@ -1,64 +1,119 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import {
 	View,
 	Image,
-	ScrollView,
 	Dimensions,
 	Text,
 	StyleSheet,
+	TouchableOpacity,
 } from 'react-native';
+import server from '../server/server';
 
 const { width } = Dimensions.get('window');
-const height = width * 0.6;
+const height = width * 0.8;
 
-export default class Swipe extends Component {
-	state = {
-		active: 0,
-	};
+// const Swipe = () => {
+// 	const [active, setActive] = useState(0);
 
-	change = ({ nativeEvent }) => {
-		const swipe = Math.ceil(
-			nativeEvent.contentOffset.x / nativeEvent.layoutMeasurement.width
-		);
-		if (swipe !== this.state.active) this.setState({ active: swipe });
-	};
+// 	// const handleScroll = ({ nativeEvent }) => {
+// 	// 	const swipe = Math.ceil(
+// 	// 		nativeEvent.contentOffset.x / nativeEvent.layoutMeasurement.width
+// 	// 	);
+// 	// 	if (swipe !== active) setActive({ active: swipe });
+// 	// };
 
-	render() {
-		return (
-			<View style={style.container}>
-				<ScrollView
-					pagingEnabled
-					horizontal
-					onScroll={this.change}
-					showsHorizontalScrollIndicator={true}
-					style={style.container}
-				>
-					{this.props.images.map((elem, index) => (
-						<Image key={index} source={{ uri: elem }} style={style.images} />
-					))}
-				</ScrollView>
-				<View style={style.paginate}>
-					{this.props.images.map((elem, index) => (
-						<Text
-							key={index}
-							style={
-								index === this.state.active
-									? style.pageActiveText
-									: style.pageText
-							}
-						>
-							⚪
-						</Text>
-					))}
-				</View>
+// 	const handlePrev = e => console.log('Previous Block');
+// 	const handleNext = e => console.log('Next Block');
+
+// 	return (
+// 		<View style={style.container}>
+// 			<Text style={style.title}>Check your next destination at Puzzle</Text>
+// 			{/* <ScrollView
+// 				pagingEnabled
+// 				horizontal
+// 				onScroll={handleScroll()}
+// 				showsHorizontalScrollIndicator={true}
+// 				style={style.container}
+// 			>
+// 				{images.map((elem, index) => (
+// 					<Image
+// 						key={index}
+// 						source={{ uri: elem.images }}
+// 						style={style.images}
+// 					/>
+// 				))}
+// 			</ScrollView>
+// 			<View style={style.paginate}>
+// 				{images.map((_, index) => (
+// 					<Text
+// 						key={index}
+// 						style={index === active ? style.pageActiveText : style.pageText}
+// 					>
+// 						⚪
+// 					</Text>
+// 				))}
+// 			</View> */}
+// 			<View style={style.btnContainer}>
+// 				<TouchableOpacity onPress={handlePrev} style={style.btn}>
+// 					<Text>Prev</Text>
+// 				</TouchableOpacity>
+// 				<TouchableOpacity onPress={handleNext} style={style.btn}>
+// 					<Text>Next</Text>
+// 				</TouchableOpacity>
+// 			</View>
+// 		</View>
+// 	);
+// };
+
+const Swipe = () => {
+	const randomImage = server[Math.floor(Math.random() * server.length)];
+	console.log('THIS IS THE IMAGE', randomImage);
+
+	const [active, setActive] = useState(0);
+
+	const handlePrev = e => console.log('Previous Block');
+	const handleNext = e => console.log('Next Block');
+
+	return (
+		<View style={style.container}>
+			<Text style={style.title}>Check your next destination at Puzzle</Text>
+			<Image source={randomImage} style={style.images} />
+			<View style={style.btnContainer}>
+				<TouchableOpacity onPress={handlePrev} style={style.btn}>
+					<Text>Prev</Text>
+				</TouchableOpacity>
+				<TouchableOpacity onPress={handleNext} style={style.btn}>
+					<Text>Next</Text>
+				</TouchableOpacity>
 			</View>
-		);
-	}
-}
-
+		</View>
+	);
+};
 const style = StyleSheet.create({
-	container: { width, height },
-	images: { width, height, resizeMode: 'cover' },
+	container: { width, height, marginTop: 40, backgroundColor: '#000000' },
+	title: {
+		display: 'flex',
+		flexDirection: 'row',
+		position: 'absolute',
+		alignSelf: 'center',
+		fontWeight: 'bold',
+		textDecorationLine: 'underline',
+		color: '#f0f8ff',
+	},
+	btnContainer: {
+		display: 'flex',
+		flexDirection: 'row',
+		position: 'relative',
+		alignContent: 'center',
+		justifyContent: 'space-evenly',
+		marginTop: 160,
+	},
+	btn: {
+		color: '#696969',
+		backgroundColor: '#f5f5dc',
+		borderRadius: 5,
+	},
+	images: { width: '50%', height: '35%', resizeMode: 'contain' },
 	paginate: {
 		display: 'flex',
 		flexDirection: 'row',
@@ -66,6 +121,8 @@ const style = StyleSheet.create({
 		bottom: 0,
 		alignSelf: 'center',
 	},
-	pageText: { color: '#888', margin: 3, fontSize: width / 30 },
-	pageActiveText: { color: '#fff', margin: 3, fontSize: width / 30 },
+	pageText: { color: '#F3F1F5', margin: 3, fontSize: width / 30 },
+	pageActiveText: { color: '#BFA2DB', margin: 3, fontSize: width / 30 },
 });
+
+export default Swipe;
